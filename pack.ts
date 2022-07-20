d15330acabe7de27c2508af486d023e33ff77861
Mohammad Mohammad <mmohammadq8@outlook.com>
2022-07-20 19:13:12 +0000

    minor changes

--- pack.ts
+++ pack.ts
@@ -32,7 +32,7 @@ const toSentimentSchema = (sentimentResponse: SentimentResponse) => {
 
 pack.addFormula({
   name: "AnalyzeSentiment",
-  description: "Get the sentiment of a given text (Positive, Negative, Neutral).",
+  description: "Get the sentiment of a given text (Positive, Negative, Neutral, Mixed, Unkown).",
   parameters: [
     coda.makeParameter({
       type: coda.ParameterType.String,
@@ -43,6 +43,15 @@ pack.addFormula({
   resultType: coda.ValueType.Object,
   schema: SentimentSchema,
   execute: async ([text], context) => {
+    if (text?.length < 3) {
+      return {
+        sentiment: 'unknown',
+        positiveScore: 0,
+        neutralScore: 0,
+        negativeScore: 0,
+      }
+    }
+
     const request: AnalysisRequest = {
       kind: "SentimentAnalysis",
       parameters: {
@@ -77,113 +86,6 @@ pack.addColumnFormat({
   instructions: "Show sentiment result of a given document",
   formulaName: "AnalyzeSentiment"
 })
-
-// A formula to fetch information about a repo.
-// pack.addFormula({
-//   name: "Repo",
-//   description: "Get information about a repo from it's URL.",
-//   parameters: [
-//     coda.makeParameter({
-//       type: coda.ParameterType.String,
-//       name: "url",
-//       description: "The URL of the repo.",
-//     }),
-//   ],
-//   resultType: coda.ValueType.Object,
-//   schema: RepoSchema,
-//   execute: async function ([url], context) {
-//     let { owner, name } = parseRepoUrl(url);
-//     let response = await context.fetcher.fetch({
-//       method: "GET",
-//       url: `https://api.github.com/repos/${owner}/${name}`,
-//     });
-//     let repo = response.body;
-//     return repo;
-//   },
-// });
-
-// A column format that automatically applies the Repo() formula.
-// pack.addColumnFormat({
-//   name: "Repo",
-//   instructions: "Show details about a GitHub repo, given a URL.",
-//   formulaName: "Repo",
-//   matchers: [RepoUrlRegex],
-// });
-
-// An action formula that allows a user to star a repo.
-// pack.addFormula({
-//   name: "Star",
-//   description: "Add a star to a repo.",
-//   parameters: [
-//     coda.makeParameter({
-//       type: coda.ParameterType.String,
-//       name: "url",
-//       description: "The URL of the repo.",
-//     }),
-//   ],
-//   resultType: coda.ValueType.Boolean,
-//   isAction: true,
-//   execute: async function ([url], context) {
-//     let { owner, name } = parseRepoUrl(url);
-//     let response = await context.fetcher.fetch({
-//       method: "PUT",
-//       url: `https://api.github.com/user/starred/${owner}/${name}`,
-//     });
-//     return true;
-//   },
-// });
-
-// A sync table that lists all of the user's repos.
-// pack.addSyncTable({
-//   name: "Repos",
-//   description: "All of the repos that the user has access to.",
-//   identityName: "Repo",
-//   schema: RepoSchema,
-//   formula: {
-//     name: "SyncRepos",
-//     description: "Sync the repos.",
-//     parameters: [],
-//     execute: async function ([], context) {
-//       // Get the page to start from.
-//       let page = (context.sync.continuation?.page as number) || 1;
-
-//       // Fetch a page of repos from the GitHub API.
-//       let url = coda.withQueryParams("https://api.github.com/user/repos", {
-//         page: page,
-//         per_page: PageSize,
-//       });
-//       let response = await context.fetcher.fetch({
-//         method: "GET",
-//         url: url,
-//       });
-//       let repos = response.body;
-
-//       // If there were some results, re-run this formula for the next page.
-//       let continuation;
-//       if (repos.length > 0) {
-//         continuation = { page: page + 1 };
-//       }
-
-//       // Return the repos and the continuation (if any).
-//       return {
-//         result: repos,
-//         continuation: continuation,
-//       };
-//     },
-//   },
-// });
-
-// A helper function that parses a repo URL and returns the owner and name.
-// function parseRepoUrl(url) {
-//   let match = url.match(RepoUrlRegex);
-//   if (!match) {
-//     throw new coda.UserVisibleError("Invalid repo URL: " + url);
-//   }
-//   return {
-//     owner: match[1],
-//     name: match[2],
-//   };
-// }
 
 function getTokenPlaceholder(token: string, context: coda.ExecutionContext): string {
   return `{{${token}-${context.invocationToken}}}`;
